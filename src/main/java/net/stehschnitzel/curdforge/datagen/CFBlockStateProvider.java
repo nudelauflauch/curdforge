fdb96d26b234bb29002b50e5101c4c3a880853c9
nudelauflauch <76126544+nudelauflauch@users.noreply.github.com>
2026-04-02 12:09:32 +0200

    added cuddly cactus

--- src/main/java/net/stehschnitzel/curdforge/datagen/CFBlockStateProvider.java
+++ src/main/java/net/stehschnitzel/curdforge/datagen/CFBlockStateProvider.java
@@ -1,9 +1,19 @@
 package net.stehschnitzel.curdforge.datagen;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.stehschnitzel.curdforge.Curdforge;
+import net.stehschnitzel.curdforge.common.blocks.CuddlyCactus;
+import net.stehschnitzel.curdforge.init.CfBlockInit;
+
+import java.util.List;
 
 public class CFBlockStateProvider extends BlockStateProvider {
 
@@ -13,6 +23,31 @@ public class CFBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
+        cuddlyCactusBlock(CfBlockInit.CUDDLY_CACTUS);
+    }
 
+    public void cuddlyCactusBlock(RegistryObject<Block> registryObject) {
+        Block cuddlyCactus = registryObject.get();
+        List<ModelFile> files = new java.util.ArrayList<>(List.of());
+        String name = name(cuddlyCactus);
+
+        for (int i = 0; i < 9; i++) {
+            files.add(models().cross(name + "_" + i,
+                    ResourceLocation.fromNamespaceAndPath(Curdforge.MOD_ID,
+                            "block/" + name + "_" +i))
+                    .renderType("cutout"));
+        }
+
+        getVariantBuilder(cuddlyCactus).forAllStates(blockState -> {
+            int age = blockState.getValue(CuddlyCactus.AGE);
+
+            return ConfiguredModel.builder()
+                    .modelFile(files.get(age))
+                    .build();
+        });
+    }
+
+    public static String name(Block block) {
+        return ForgeRegistries.BLOCKS.getKey(block).getPath();
     }
 }
--- src/main/java/net/stehschnitzel/curdforge/init/CfBlockInit.java
+++ src/main/java/net/stehschnitzel/curdforge/init/CfBlockInit.java
@@ -1,16 +1,22 @@
 package net.stehschnitzel.curdforge.init;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.stehschnitzel.curdforge.Curdforge;
+import net.stehschnitzel.curdforge.common.blocks.CuddlyCactus;
 
 public class CfBlockInit {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
 			Curdforge.MOD_ID);
 
-
+    public static final RegistryObject<Block> CUDDLY_CACTUS = BLOCKS.register("cuddly_cactus",
+            () -> new CuddlyCactus(BlockBehaviour.Properties.copy(Blocks.CACTUS).noCollission())
+    );
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);
